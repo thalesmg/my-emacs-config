78beb5f46db686cbc6a2124a3512b47bf141c4ea
Thales Macedo Garitezi <tgaritezi@xerpa.com.br>
2017-05-31 18:54:23 -0300

    Removes f***ing org-notify
    ... It made my minibuffer loose focus, duplicated Helm windows...

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -10,7 +10,7 @@
             (add-hook 'auto-save-hook 'org-save-all-org-buffers nil t)
             (auto-save-mode)))
 
-(require 'org-alert)
-(setq org-alert-interval 300)
-(org-alert-enable)
-(setq alert-default-style 'libnotify)
+;; (require 'org-alert)
+;; (setq org-alert-interval 300)
+;; (org-alert-enable)
+;; (setq alert-default-style 'libnotify)
