50ff4d7e931405956322a5d1666d1af24b8f6062
Thales Macedo Garitezi <thalesmg@gmail.com>
2017-10-18 21:48:05 -0200

    Adds hlint to intero.

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -1,4 +1,8 @@
+(require 'intero)
+(require 'flycheck)
+
 ;; Intero
 (add-hook 'haskell-mode-hook 'intero-mode)
 
-
+(with-eval-after-load 'intero
+  (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))
