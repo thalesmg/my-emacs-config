86cc8c1834eb36f930aabeb79d39a72ccd3c1ce6
Thales Macedo Garitezi <thalesmg@gmail.com>
2018-08-26 11:12:00 -0300

    Custom helm-do-ag and requires dired.

--- customizations/setup-ripgrep-projectile.el
+++ customizations/setup-ripgrep-projectile.el
@@ -1,8 +1,19 @@
 (require 'projectile-ripgrep)
+(require 'helm)
+(require 'helm-projectile)
 (require 'helm-ag)
 (require 'rg)
 
-(define-key projectile-mode-map (kbd "C-c p s r") 'projectile-ripgrep)
+(define-key projectile-mode-map (kbd "C-c p p") 'helm-projectile-switch-project)
+;; (define-key projectile-mode-map (kbd "C-c p s r") 'projectile-ripgrep)
+
+(defun tmg-helm-do-rg
+    (&optional targets)
+  (interactive)
+  (let ((basedir (projectile-project-root)))
+   (helm-do-ag basedir targets)))
+(define-key projectile-mode-map (kbd "C-c p s r") 'tmg-helm-do-rg)
+
 
 (custom-set-variables
  '(helm-ag-base-command "rg --no-heading")
