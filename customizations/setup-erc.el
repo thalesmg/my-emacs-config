2bf6f9149b8d99f59a83f50e29dbc2764e95e8da
Thales Macedo Garitezi <tgaritezi@xerpa.com.br>
2018-10-05 15:09:13 -0300

    Erc'''

--- customizations/setup-erc.el
+++ customizations/setup-erc.el
@@ -3,40 +3,46 @@
 (setq erc-hide-list '("JOIN" "PART" "QUIT" "AWAY"))
 (setq erc-track-exclude-types '("MODE" "AWAY"))
 
-;; https://www.emacswiki.org/emacs/ErcNickColors#toc2
-;; Pool of colors to use when coloring IRC nicks.
-(setq erc-colors-list '("green" "blue" "red"
-			"dark gray" "dark orange"
-			"dark magenta" "maroon"
-			"indian red" "black" "forest green"
-			"midnight blue" "dark violet"))
-;; special colors for some people
-;; (setq erc-nick-color-alist '(("John" . "blue")
-;; 			     ("Bob" . "red")
-;; 			     ))
-(setq erc-nick-color-alist '())
+;; https://www.emacswiki.org/emacs/ErcNickColors#toc5
 
-(defun erc-get-color-for-nick (nick)
-  "Gets a color for NICK. If NICK is in erc-nick-color-alist, use that color, else hash the nick and use a random color from the pool"
-  (or (cdr (assoc nick erc-nick-color-alist))
-      (nth
-       (mod (string-to-number
-	     (substring (md5 (downcase nick)) 0 6) 16)
-	    (length erc-colors-list))
-       erc-colors-list)))
+(defmacro unpack-color (color red green blue &rest body)
+  `(let ((,red   (car ,color))
+         (,green (car (cdr ,color)))
+         (,blue  (car (cdr (cdr ,color)))))
+     ,@body))
 
-(defun erc-put-color-on-nick ()
-  "Modifies the color of nicks according to erc-get-color-for-nick"
+(defun rgb-to-html (color)
+  (unpack-color color red green blue
+   (concat "#" (format "%02x%02x%02x" red green blue))))
+
+(defun hexcolor-luminance (color)
+  (unpack-color color red green blue
+   (floor (+ (* 0.299 red) (* 0.587 green) (* 0.114 blue)))))
+
+(defun invert-color (color)
+  (unpack-color color red green blue
+   `(,(- 255 red) ,(- 255 green) ,(- 255 blue))))
+
+(defun erc-get-color-for-nick (nick dark)
+  (let* ((hash     (md5 (downcase nick)))
+         (red      (mod (string-to-number (substring hash 0 10) 16) 256))
+         (blue     (mod (string-to-number (substring hash 10 20) 16) 256))
+         (green    (mod (string-to-number (substring hash 20 30) 16) 256))
+         (color    `(,red ,green ,blue)))
+    (rgb-to-html (if (if dark (< (hexcolor-luminance color) 85)
+                       (> (hexcolor-luminance color) 170))
+                     (invert-color color)
+                   color))))
+
+(defun erc-highlight-nicknames ()
   (save-excursion
     (goto-char (point-min))
-    (while (forward-word 1)
-      (setq bounds (bounds-of-thing-at-point 'word))
-      (setq word (buffer-substring-no-properties
-                  (car bounds) (cdr bounds)))
-      (when (or (and (erc-server-buffer-p) (erc-get-server-user word))
-                (and erc-channel-users (erc-get-channel-user word)))
-        (put-text-property (car bounds) (cdr bounds)
-                           'face (cons 'foreground-color
-                                       (erc-get-color-for-nick word)))))))
+    (while (re-search-forward "\\w+" nil t)
+      (let* ((bounds (bounds-of-thing-at-point 'symbol))
+             (nick   (buffer-substring-no-properties (car bounds) (cdr bounds))))
+        (when (erc-get-server-user nick)
+          (put-text-property
+           (car bounds) (cdr bounds) 'face
+           (cons 'foreground-color (erc-get-color-for-nick nick 't))))))))
 
-(add-hook 'erc-insert-modify-hook 'erc-put-color-on-nick)
+(add-hook 'erc-insert-modify-hook 'erc-highlight-nicknames)
