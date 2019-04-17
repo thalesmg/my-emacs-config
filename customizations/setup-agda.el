306d7fa82b9a575dbcb98b49195f67f1e76e79f9
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-04-16 22:15:49 -0300

    setup agda

---
+++ customizations/setup-agda.el
@@ -0,0 +1,9 @@
+(require 'agda2-mode)
+
+(custom-set-faces
+ '(agda2-highlight-datatype-face ((t (:foreground "light blue"))))
+ '(agda2-highlight-function-face ((t (:foreground "light blue"))))
+ '(agda2-highlight-postulate-face ((t (:foreground "light blue"))))
+ '(agda2-highlight-primitive-face ((t (:foreground "light blue"))))
+ '(agda2-highlight-primitive-type-face ((t (:foreground "light blue"))))
+ '(agda2-highlight-record-face ((t (:foreground "light blue")))))
