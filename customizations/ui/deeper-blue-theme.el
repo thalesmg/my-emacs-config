5ec91dd54886b7fe4fcfd97f885ecb6c568c6879
Thales Macedo Garitezi <thales@zenfinance.com.br>
2019-06-07 10:31:32 -0300

    ui customizations deeper-blue clojure

--- customizations/ui/deeper-blue-theme.el
+++ customizations/ui/deeper-blue-theme.el
@@ -1,4 +1,7 @@
 (custom-theme-set-faces
   'deeper-blue
   '(font-lock-comment-face ((t (:foreground "gray50" :slant italic))))
-  '(font-lock-doc-face ((t (:foreground "#787878" :slant italic)))))
+  '(font-lock-doc-face ((t (:foreground "#787878" :slant italic))))
+  '(clojure-keyword-face ((t (:inherit font-lock-constant-face))))
+  '(font-lock-constant-face ((t (:foreground "orange1"))))
+  '(font-lock-type-face ((t (:foreground "gold")))))
