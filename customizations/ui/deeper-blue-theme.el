30b875e7844067d54328871910fecdfe90c8c5bb
Thales Macedo Garitezi <thales@zenfinance.com.br>
2019-07-10 13:59:26 -0300

    uma cor do org-mode

--- customizations/ui/deeper-blue-theme.el
+++ customizations/ui/deeper-blue-theme.el
@@ -4,4 +4,5 @@
   '(font-lock-doc-face ((t (:foreground "#787878" :slant italic))))
   '(clojure-keyword-face ((t (:inherit font-lock-constant-face))))
   '(font-lock-constant-face ((t (:foreground "orange1"))))
-  '(font-lock-type-face ((t (:foreground "gold")))))
+  '(font-lock-type-face ((t (:foreground "gold"))))
+  '(org-code ((t (:foreground "green yellow")))))
