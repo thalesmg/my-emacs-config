178f87f2e0f0690e57860232f01efb58b49dba7b
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-09-12 16:25:50 -0300

    C-s -> helm-swoop

--- customizations/editing.el
+++ customizations/editing.el
@@ -1,5 +1,7 @@
 ;; Customizations relating to editing a buffer.
 
+(require 'helm-swoop)
+
 ;; Key binding to use "hippie expand" for text autocompletion
 ;; http://www.emacswiki.org/emacs/HippieExpand
 (global-set-key (kbd "M-/") 'hippie-expand)
@@ -23,7 +25,7 @@
 
 ;; Interactive search key bindings. By default, C-s runs
 ;; isearch-forward, so this swaps the bindings.
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-s") 'helm-swoop)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
