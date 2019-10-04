089ee7aae17137efd03da4512fc227a08524c461
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-10-04 10:08:43 -0300

    disable startup screen

--- customizations/ui.el
+++ customizations/ui.el
@@ -6,6 +6,8 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
 
+(setq inhibit-startup-screen t)
+
 (defun disable-all-themes ()
   "disables all themes"
   (dolist (x custom-enabled-themes)
