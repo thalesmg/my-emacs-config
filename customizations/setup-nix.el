3dd6106b1d7566e6c9248b29e439511a104a94a1
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-01-20 20:04:56 -0300

    nix-fmt

--- customizations/setup-nix.el
+++ customizations/setup-nix.el
@@ -1,3 +1,12 @@
 (require 'nix-mode)
 
 (add-hook 'nix-mode-hook 'company-mode)
+
+(define-key nix-mode-map (kbd "C-x M-q")
+  (lambda ()
+    (interactive)
+    (basic-save-buffer)
+    (message projectile-project-root)
+    (let ((curr-file-path (string-remove-prefix (projectile-project-root) buffer-file-name)))
+      (shell-command (concat "nixfmt " curr-file-path))
+      (revert-buffer nil t))))
