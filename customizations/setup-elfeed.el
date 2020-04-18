03d9e49480d666c72cedeae1b2b425030c1b6663
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-04-18 13:38:20 -0300

    hackernews elfeed

--- customizations/setup-elfeed.el
+++ customizations/setup-elfeed.el
@@ -4,4 +4,5 @@
       '("https://haskellweekly.news/newsletter.atom"
         "http://alan.petitepomme.net/cwn/cwn.rss"
         "https://weekly.nixos.org/feeds/all.rss.xml"
-        "https://this-week-in-rust.org/atom.xml"))
+        "https://this-week-in-rust.org/atom.xml"
+        "https://us1.campaign-archive.com/feed?u=faa8eb4ef3a111cef92c4f3d4&id=e505c88a2e"))
