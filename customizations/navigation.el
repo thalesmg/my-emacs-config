be2462701e3da9362f20429a61e2cd07b50d890d
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-12-26 20:01:24 -0300

    experimentando ace-window de novo

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -49,7 +49,8 @@
 ;; (global-set-key (kbd "C-x o") 'ace-window)
 ;; switch-window
 (require 'switch-window)
-(global-set-key (kbd "C-x o") 'switch-window)
+(global-set-key (kbd "C-x o") 'ace-window)
+;; (global-set-key (kbd "C-x o") 'switch-window)
 (global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
 (global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
 (global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
