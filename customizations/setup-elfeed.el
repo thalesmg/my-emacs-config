83c7e3336819c9a506680ee9cd0aac9fd1b8e473
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-07-01 08:40:46 -0300

    fix elfeed

--- customizations/setup-elfeed.el
+++ customizations/setup-elfeed.el
@@ -2,16 +2,18 @@
 (require 'elfeed-org)
 
 
-(use-package elfeed
-      :ensure t
-      :config
-      (setq
-            ;; elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory)
-            elfeed-show-entry-switch 'display-buffer))
-
 (use-package elfeed-org
   :ensure t
   :config
   (setq elfeed-show-entry-switch 'display-buffer)
   (setq rmh-elfeed-org-files
         (list "~/org/diversos/elfeed.org")))
+
+(use-package elfeed
+  :ensure t
+  :init
+  (elfeed-org)
+  :config
+  (setq
+   ;; elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory)
+   elfeed-show-entry-switch 'display-buffer))
