1918ead4ab3ea0b6bdeabcf04f70c995a55d9164
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-08-23 10:13:45 -0300

    racket mode improvement

--- customizations/setup-racket.el
+++ customizations/setup-racket.el
@@ -2,4 +2,5 @@
 
 (add-hook 'racket-mode-hook 'enable-paredit-mode)
 (add-hook 'racket-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'racket-mode-hook 'flycheck-mode)
+;; (add-hook 'racket-mode-hook 'flycheck-mode)
+(add-hook 'racket-mode-hook 'racket-xp-mode)
