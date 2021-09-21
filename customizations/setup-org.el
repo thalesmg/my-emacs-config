61dac8a68a9475d3c459c60afaed38342df68598
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-09-21 14:05:52 -0300

    org-roam fns and macro experiment

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -161,6 +161,20 @@ to update the dynamic block."
       (goto-char beg)
       (org-table-insert-hline))))
 
+(defmacro tmg/--wrap-org-roam-fn (fn)
+  (let ((fn-name (intern (concat "tmg/" (symbol-name fn)))))
+   `(defun ,fn-name ()
+      (interactive)
+      (let ((ivy-mode-before ivy-mode)))
+      (unwind-protect
+          (progn
+            (ivy-mode +1)
+            (,fn))
+        (ivy-mode ivy-mode-before)))))
+
+(tmg/--wrap-org-roam-fn org-roam-node-find)
+(tmg/--wrap-org-roam-fn org-roam-node-insert)
+
 (use-package org-roam
   :init
   (setq org-roam-v2-ack t)
