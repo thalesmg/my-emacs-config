8fdb98d20469e112b9517f56510311e3794389b2
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-10-31 08:14:14 -0300

    rust lsp

--- customizations/setup-rust.el
+++ customizations/setup-rust.el
@@ -2,7 +2,8 @@
 (require 'rust-mode)
 (require 'flycheck-rust)
 
-(add-hook 'rust-mode-hook 'racer-mode)
+;; (add-hook 'rust-mode-hook 'racer-mode)
+(add-hook 'rust-mode-hook #'lsp)
 (add-hook 'rust-mode-hook 'flycheck-mode)
 (add-hook 'rust-mode-hook #'flycheck-rust-setup)
 (add-hook 'rust-mode-hook 'company-mode)
