20e4ff93f196baa7389846a213bb54a271315ec4
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-07-05 13:34:30 -0300

    better erlang binary bracket pairs

--- customizations/setup-erlang.el
+++ customizations/setup-erlang.el
@@ -1,1 +1,8 @@
+(require 'smartparens)
+
 (add-hook 'erlang-mode-hook #'lsp)
+
+;; https://github.com/JimMoen/Emacs-Config/commit/2d1305fb6042d65821954aeb7758576967d3c051
+(sp-with-modes '(erlang-mode)
+  (sp-local-pair "<<" ">>")
+  (sp-local-pair "<<\"" "\">>"))
