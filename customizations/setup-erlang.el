f5f3acc8196dbd70784de5e748a4f92ea8f4825f
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-07-10 13:54:25 -0300

    smartparens for erlang

--- customizations/setup-erlang.el
+++ customizations/setup-erlang.el
@@ -1,6 +1,7 @@
 (require 'smartparens)
 
 (add-hook 'erlang-mode-hook #'lsp)
+(add-hook 'erlang-mode-hook 'smartparens-mode)
 
 ;; https://github.com/JimMoen/Emacs-Config/commit/2d1305fb6042d65821954aeb7758576967d3c051
 (sp-with-modes '(erlang-mode)
